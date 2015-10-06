7ed343b0b0806245b87e5b6400b3e1d56166231d
Shane Richards <shane.richards121@gmail.com>
2015-10-06 00:02:11 -0500

    initial commit

---
+++ src/Message.java
@@ -0,0 +1,47 @@
+/**
+ * Created by shane on 10/5/15.
+ */
+public class Message
+{
+    private String data;
+
+    public Message(String inputData)
+    {
+        if (inputData == null)
+        {
+            data = "";
+        }
+        else if (inputData.length() > NetworkSimulator.MAXDATASIZE)
+        {
+            data = "";
+        }
+        else
+        {
+            data = new String(inputData);
+        }
+    }
+
+    public boolean setData(String inputData)
+    {
+        if (inputData == null)
+        {
+            data = "";
+            return false;
+        }
+        else if (inputData.length() > NetworkSimulator.MAXDATASIZE)
+        {
+            data = "";
+            return false;
+        }
+        else
+        {
+            data = new String(inputData);
+            return true;
+        }
+    }
+
+    public String getData()
+    {
+        return data;
+    }
+}
